e024c87376a0ec74c9dd75c6c4a6b422009f8663
prasanthkumar494 <prasanth.kumar@adroitent.com>
2024-10-18 18:45:02 +0530

    new class ex2 is added

---
+++ AD.Inventory.Core/InventoryEx2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AD.Inventory.Core
+{
+    public class InventoryEx2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
